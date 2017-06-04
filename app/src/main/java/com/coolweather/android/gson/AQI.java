93d6875e83aa24e791d64154ed2ade8e71ad479d
yuanjiantian <yuanjiantian.dev@foxmail.com>
2017-06-05 01:38:49 +0800

    加入显示天气信息的功能

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,16 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by surface on 2017/6/3.
+ */
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity{
+        public String aqi;
+        public String pm25;
+    }
+
+}
